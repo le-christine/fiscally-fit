a10eca3d408cf446e1ddb28801b7e0e39468a814
Christine Le <christine.le244@gmail.com>
2023-05-17 15:08:38 -0400

    fix(taxService): Add zero income corner case and test file

--- app/services/taxService.go
+++ app/services/taxService.go
@@ -9,10 +9,10 @@ import (
 // Responsible for retrieving tax brackets from the mock API and calculating the taxes owed.
 
 type TaxService struct {
-	TaxBrackets map[int]models.TaxBrackets // Use the updated struct name here
+	TaxBrackets map[int]models.TaxBrackets
 }
 
-func NewTaxService(brackets map[int]models.TaxBrackets) *TaxService { // Use the updated struct name here
+func NewTaxService(brackets map[int]models.TaxBrackets) *TaxService {
 	return &TaxService{
 		TaxBrackets: brackets,
 	}
@@ -23,14 +23,13 @@ func (s *TaxService) CalculateTaxes(income float64, year int) (map[string]interf
 
 	// Vars to return
 	var totalTax, prev, max float64
-	taxesByBracket := make(map[string]float64)
+	taxesByBracket := make(map[string]string)
 
 	//Find the tax brackets for the given year
 	brackets, ok := s.TaxBrackets[year]
 	if !ok {
 		return nil, fmt.Errorf("no tax brackets found for year %d", year)
 	}
-
 
 	for _, bracket := range brackets.TaxBrackets {
 
@@ -42,23 +41,28 @@ func (s *TaxService) CalculateTaxes(income float64, year int) (map[string]interf
 
 		if income <= max || max == 0 {
 			taxableAmount := math.Min(income-bracket.Min, max-bracket.Min)
-			taxesByBracket[fmt.Sprintf("Tax band: $%f to $%f", bracket.Min, max)] = taxableAmount
+			taxesByBracket[fmt.Sprintf("Tax band: $%.2f to $%.2f", bracket.Min, max)] = fmt.Sprintf("$%.2f", taxableAmount)
 			totalTax += taxableAmount * bracket.Rate
 			break
 		} else {
 			taxableAmount := max - prev
-			taxesByBracket[fmt.Sprintf("Tax band: $%f to $%f", bracket.Min, max)] = taxableAmount
+			taxesByBracket[fmt.Sprintf("Tax band: $%.2f to $%.2f", bracket.Min, max)] = fmt.Sprintf("$%.2f", taxableAmount)
 			totalTax += taxableAmount * bracket.Rate
 			prev = max
 		}
 	}
 
-	taxRate := totalTax/income*100
+	var taxRate float64
+	if income == 0 {
+		taxRate = 0
+	} else {
+		taxRate = totalTax / income * 100
+	}
 
 	response := map[string]interface{}{
-		"totalTaxes": totalTax,
-		"effectiveTaxRate": taxRate,
-		"taxesByBracket": taxesByBracket,
+		"totalTaxes":       fmt.Sprintf("$%.2f", totalTax),
+		"effectiveTaxRate": math.Round(taxRate),
+		"taxesByBracket":   taxesByBracket,
 	}
 
 	return response, nil
--- app/services/taxService_test.go
+++ app/services/taxService_test.go
@@ -1,1 +1,28 @@
 package services
+
+import (
+	"fiscally-fit/app/models"
+	"reflect"
+	"testing"
+)
+
+func TestCalculateTaxes(t *testing.T) {
+	taxService := NewTaxService(models.AllTaxBrackets)
+
+	calculatedTaxes, _ := taxService.CalculateTaxes(100000, 2020)
+
+	expectedTaxes := map[string]interface{}{
+		"totalTaxes":       "$17991.78",
+		"effectiveTaxRate": 18,
+		"taxesByBracket": map[string]string{
+			"Tax band: $0.00 to $48535.00":      "$48535.00",
+			"Tax band: $48535.00 to $97069.00":  "$48534.00",
+			"Tax band: $97069.00 to $150473.00": "$2931.00",
+		},
+	}
+
+	if reflect.DeepEqual(calculatedTaxes, expectedTaxes) {
+		t.Errorf("calculatedTaxes = %v; want %v", calculatedTaxes, expectedTaxes)
+
+	}
+}
